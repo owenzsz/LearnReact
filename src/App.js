9d68f39131e853135af2e47def7156b736a08474
Owen Zhang <sizhengzhang2021@u.northwestern.edu>
2019-10-29 01:19:56 -0500

    keep progress

--- src/App.js
+++ src/App.js
@@ -4,9 +4,89 @@ import { Button, Container, Title } from 'rbx';
 import './App.css'
 import './bulma.min.css'
 
+
+const UseSelection = () => {
+  const [selected, setSelected] = useState([]);
+  const [addedTimes, setAddedTimes] = useState(0);
+  //add product to cart
+  const addToCart = (x, add = true) => {
+    if (add) {
+      setSelected([x].concat(selected));
+    } else {
+      selected.splice(selected.indexOf(x), 1);
+      setSelected(selected);
+    }
+  };
+  return [ selected, addToCart ];
+};
+
+const buttonColor = selected => (
+  selected ? 'success' : 'null'
+);
+
+const ShoppingCart =({products, selectState}) =>(
+  null
+)
+
+const ProductsList = ({products}) => (
+  <div className='products-list columns is-multiline'>
+    {products.map(product => <Product product={product} />)}
+  </div>
+ 
+)
+
+const Product = ({product, selectState}) =>(
+  <Container className='column is-3 is-centered'>
+    <div className='product-card'>
+      <img src={getProductImageURL(product)} ></img>
+      <div class='product-info'>
+        <div class='product-name'>{getProductName(product)}</div>
+        <div class='product-description'> {getProductDescription(product)}</div>
+        <div class='product-price'> {getProductPrice(product)}</div>
+
+        <Button.Group class='product-sizes'>
+          <Button>S</Button>
+          <Button>M</Button>
+          <Button>L</Button>
+          <Button>XL</Button>
+        </Button.Group>
+      </div>
+
+      <Button onClick={()=>{selectState.addToCart(product, true);}}>Add to cart</Button>
+    </div>
+  </Container>
+);
+
+
+
+const getProductName = product => (
+  product.title
+);
+
+const getProductPrice = product =>(
+  product.price
+);
+const getProductDescription = product =>{
+  if(product.description){
+    return product.description
+  }
+  else{
+    return 'No description'
+  }
+  
+}
+const getProductImageURL = product =>(
+  './data/products/'+product.sku+'_1.jpg'
+)
+
 const App = () => {
   const [data, setData] = useState({});
+  const [inventoryData, setInventoryData] = useState({});
+  const [selected, addToCart] = UseSelection();
+  const [state, setState] = useState({right: false});
+
   const products = Object.values(data);
+  const inventory = Object.values(inventoryData);
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -16,6 +96,7 @@ const App = () => {
     fetchProducts();
   }, []);
 
+
   return (
     <Container>
       <ProductsList products={products} />
@@ -23,34 +104,6 @@ const App = () => {
   );
 };
 
-const ProductsList = ({products}) => (
-  <div className='products-list columns is-multiline'>
-    {products.map(product => <Product product={product} />)}
-  </div>
-  
-)
 
-const Product = ({product}) =>(
-  <Container className='column is-3 is-centered'>
-    <div className='product-card'>
-      <img src={getProductImageURL(product)} ></img>
-      <div class='product-name'>{getProductName(product)}</div>
-      <div class='product-price'> {getProductPrice(product)}</div>
-      <Button>Add to cart</Button>
-    </div>
-  </Container>
-);
-
-const getProductName = product => (
-  product.title
-);
-
-const getProductPrice = product =>(
-  product.price
-);
-
-const getProductImageURL = product =>(
-  './data/products/'+product.sku+'_1.jpg'
-)
 
 export default App;
